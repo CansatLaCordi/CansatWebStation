da6fe8a7ee6353cd6e709e76d27eaf3ddd0b666f
Adrian Francisco Gonzalez Gutierrez <guanoman_adrian@hotmail.com>
2014-08-05 01:46:48 -0500

    Setebos website, structure and flights catalog

---
+++ Cansat.Setebos/Cansat.Setebos.Database/dbo/Tables/Data.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Data] (
+    [DataId]              INT              IDENTITY (1, 1) NOT NULL,
+    [FlightId]            INT              NOT NULL,
+    [Datetime]            DATETIME         NULL,
+    [Temperature]         DECIMAL (18, 8)  NULL,
+    [Latitude]            DECIMAL (18, 10) NULL,
+    [Longitude]           DECIMAL (18, 10) NULL,
+    [Altitude]            DECIMAL (18, 8)  NULL,
+    [Humidity]            DECIMAL (18, 8)  NULL,
+    [Presure]             DECIMAL (18, 8)  NULL,
+    [Voltage]             DECIMAL (18, 8)  NULL,
+    [CO]                  DECIMAL (18, 10) NULL,
+    [InternalTemperature] DECIMAL (18, 8)  NULL,
+    CONSTRAINT [PK_Data] PRIMARY KEY CLUSTERED ([DataId] ASC),
+    CONSTRAINT [FK_Data_Flights] FOREIGN KEY ([FlightId]) REFERENCES [dbo].[Flights] ([FlightId])
+);
+
---
+++ Cansat.Setebos/Cansat.Setebos.Database/dbo/Tables/Flights.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Flights] (
+    [FlightId] INT           IDENTITY (1, 1) NOT NULL,
+    [Active]   BIT           NULL,
+    [Name]     NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Flights] PRIMARY KEY CLUSTERED ([FlightId] ASC)
+);
+
